c9accd30ff4d1e704c09c951ae1b0e101bde83fa
Patrick Brewster <Patrick.shepherd33@gmail.com>
2026-07-26 00:34:44 -0400

    Add a BusinessLogicLayer and finish the logic to add a new verse

--- FileIOAndLINQ/PresentationLayer/FrmVerseList.cs
+++ FileIOAndLINQ/PresentationLayer/FrmVerseList.cs
@@ -7,6 +7,7 @@
  */
 
 using FileIOAndLINQ.Models;
+using FileIOAndLINQ.Services.BusinessLogicLayer;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -26,6 +27,12 @@ namespace FileIOAndLINQ.PresentationLayer
         bool isValidBook = false, isValidChapter = false,
              isValidVerse = false, isValidText = false,
              isValidMeaning = false, isValidImportance = false;
+        // Business logic variable
+        private VerseLogic _verseLogic;
+
+        /// <summary>
+        /// Default constructor for FrmVerseList
+        /// </summary>
         public FrmVerseList()
         {
             InitializeComponent();
@@ -33,6 +40,8 @@ namespace FileIOAndLINQ.PresentationLayer
             InitializeErrors();
             // Initialize cmbVerseBook
             InitializeBooks();
+            // Initialize the verse logic variable
+            _verseLogic = new VerseLogic();
         }
         /// <summary>
         /// Initialize the errors list
@@ -313,6 +322,10 @@ namespace FileIOAndLINQ.PresentationLayer
                     txtVerseText.Text,
                     txtVerseMeaning.Text,
                     (int)nudVerseImportance.Value);
+                // Add the new verse using the _verseLogic variable
+                _verseLogic.AddVerse(verse);
+                // Clear the input fields
+                ClearInputFields();
             }
             else if (!isValidBook)
             {
@@ -339,6 +352,20 @@ namespace FileIOAndLINQ.PresentationLayer
                 lblImportanceError.Visible = true;
             }
         } // End of BtnAddVerseClickEH
+
+        public void ClearInputFields()
+        {
+            // Clear the book combo box
+            cmbVerseBook.SelectedIndex = -1;
+            // Clear the textboxes in grpAddVerse
+            foreach (TextBox textBox in grpAddVerse.Controls.OfType<TextBox>())
+            {
+                // Clear the textbox
+                textBox.Clear();
+            }
+            // Reset the numeric up-down control
+            nudVerseImportance.Value = 0;
+        }
     }
 }
 
---
+++ FileIOAndLINQ/Services/BusinessLogicLayer/VerseLogic.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Patrick Brewster
+ * CST - 250
+ * 07/25/2026
+ * File I/O and LINQ
+ * Activity 6
+ */
+
+using FileIOAndLINQ.Models;
+using FileIOAndLINQ.Services.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileIOAndLINQ.Services.BusinessLogicLayer
+{
+    class VerseLogic
+    {
+        // Class level variables
+        private VerseDAO _verseDAO;
+
+        /// <summary>
+        /// Default constructor for VerseLogic
+        /// </summary>
+        public VerseLogic()
+        {
+            // Initalize the data access object
+            _verseDAO = new VerseDAO();
+        }
+
+        public int AddVerse(VerseRequestModel verse)
+        {
+            // Return the DAO method
+            return _verseDAO.AddVerse(verse);
+        }
+    }
+}
